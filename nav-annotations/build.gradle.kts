c5a14bd6e5e348020cfcd0cde4c9566f1a38bbfd
gongleilei <gll_android@163.com>
2025-10-21 14:00:20 +0800

    update README.md

--- nav-annotations/build.gradle.kts
+++ nav-annotations/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     id("maven-publish")
+    alias(libs.plugins.maven.publish.plugin)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -21,49 +22,35 @@ dependencies {
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
+mavenPublishing {
+    coordinates("pub.gll", "nav-annotations", providers.gradleProperty("gllNavPushVersion").get())
+    publishToMavenCentral()
+    signAllPublications()
 
-                groupId = "pub.gll"
-                artifactId = "nav-annotations"
-                version = providers.gradleProperty("gllNavPushVersion").get()
+    pom {
+        name.set("nav-annotations")
+        description.set("[zh]GNav 路由框架，自动注册，自动生成 goXXX 拓展函数，自动生成 getXXXRoute 函数，支持拦截器 [en]GNav routing framework, automatic registration, automatic generation of goXXX extension functions, automatic generation of getXXXRoute functions, support for interceptors")
+        inceptionYear.set("2025")
+        url.set("https://github.com/a765032380/GNav")
 
-                pom {
-                    name.set("nav-annotations")
-                    description.set("nav-annotations")
-                    url.set("http://gll.pub")
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("gongleilei")
-                            name.set("gongleilei")
-                            email.set("gll_android@163.com")
-                        }
-                    }
-                }
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
 
-        repositories {
-            maven {
-                name = "NexusPublic"
-                url = uri("http://gll.pub:8081/repository/maven-releases/")
-                isAllowInsecureProtocol = true
-                credentials {
-                    username = "admin"
-                    password = "GLL_android"
-                }
+        developers {
+            developer {
+                id.set("gll")
+                name.set("gll")
+                email.set("gll_android@163.com")
             }
+        }
+        scm {
+            url.set("https://github.com/a765032380/GNav")
+            connection.set("scm:git:https://github.com/a765032380/GNav.git")
+            developerConnection.set("scm:git:ssh://git@github.com:a765032380/GNav.git")
         }
     }
 }
--- nav/build.gradle.kts
+++ nav/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("maven-publish")
+    alias(libs.plugins.maven.publish.plugin)
 }
 
 android {
@@ -56,50 +57,35 @@ dependencies {
 }
 
 
+mavenPublishing {
+    coordinates("pub.gll", "nav", providers.gradleProperty("gllNavPushVersion").get())
+    publishToMavenCentral()
+    signAllPublications()
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["release"])
+    pom {
+        name.set("nav")
+        description.set("[zh]GNav 路由框架，自动注册，自动生成 goXXX 拓展函数，自动生成 getXXXRoute 函数，支持拦截器 [en]GNav routing framework, automatic registration, automatic generation of goXXX extension functions, automatic generation of getXXXRoute functions, support for interceptors")
+        inceptionYear.set("2025")
+        url.set("https://github.com/a765032380/GNav")
 
-                groupId = "pub.gll"
-                artifactId = "nav"
-                version = providers.gradleProperty("gllNavPushVersion").get()
-
-                pom {
-                    name.set("nav")
-                    description.set("nav")
-                    url.set("http://gll.pub")
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("gongleilei")
-                            name.set("gongleilei")
-                            email.set("gll_android@163.com")
-                        }
-                    }
-                }
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
 
-        repositories {
-            maven {
-                name = "NexusPublic"
-                url = uri("http://gll.pub:8081/repository/maven-releases/")
-                isAllowInsecureProtocol = true
-                credentials {
-                    username = "admin"
-                    password = "GLL_android"
-                }
+        developers {
+            developer {
+                id.set("gll")
+                name.set("gll")
+                email.set("gll_android@163.com")
             }
+        }
+        scm {
+            url.set("https://github.com/a765032380/GNav")
+            connection.set("scm:git:https://github.com/a765032380/GNav.git")
+            developerConnection.set("scm:git:ssh://git@github.com:a765032380/GNav.git")
         }
     }
 }
